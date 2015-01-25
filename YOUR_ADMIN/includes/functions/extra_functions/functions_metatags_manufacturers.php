7b7b7ac69c29077855e69de431883ff80de8f3e5
lat9 <lat9@vinosdefrutastropicales.com>
2015-01-25 11:46:44 -0500

    Admin extra functions updates
    Incorporate the installation SQL script into the admin-level
    extra_functions file.   Since there's SQL caching now in the base Zen
    Cart, use the same SQL statement for each of the functions' inputs.

--- YOUR_ADMIN/includes/functions/extra_functions/functions_metatags_manufacturers.php
+++ YOUR_ADMIN/includes/functions/extra_functions/functions_metatags_manufacturers.php
@@ -8,43 +8,38 @@
  * @version $Id: functions_metatags_manufacturers.php 2844 2006-01-13 06:46:29Z drbyte $
  * @no-docs
  */
-
+// -----
+// Create database table, if it doesn't already exist.
+//
+$db->Execute ("CREATE TABLE IF NOT EXISTS " . TABLE_MANUFACTURERS_META . " (
+  `manufacturers_id` int(11) NOT NULL,
+  `language_id` int(11) NOT NULL default '1',
+  `metatags_title` varchar(255) NOT NULL default '',
+  `metatags_keywords` text,
+  `metatags_description` text,
+  PRIMARY KEY  (`manufacturers_id`,`language_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=" . DB_CHARSET);
 
 /**
  * Manufacturers specific metatags
  */
-  function zen_get_manufacturer_metatags_manu_title($manufacturer_id, $language_id) {
-    global $db;
-    $category = $db->Execute("select metatags_title
-                              from ".TABLE_MANUFACTURERS_META.
-                              " where manufacturers_id = '" . (int)$manufacturer_id . "'
-                              and language_id = '" . (int)$language_id . "'");
+function zen_get_manufacturer_metatags_title ($manufacturer_id, $language_id) {
+  global $db;
+  $category = $db->Execute ("SELECT * FROM " . TABLE_MANUFACTURERS_META . " WHERE manufacturers_id = " . (int)$manufacturer_id . " AND language_id = " . (int)$language_id . " LIMIT 1");
+  
+  return ($category->EOF) ? '' : $category->fields['metatags_title'];
+}
 
-    return $category->fields['metatags_title'];
-  }
+function zen_get_manufacturer_metatags_description ($manufacturer_id, $language_id) {
+  global $db;
+  $category = $db->Execute ("SELECT * FROM " . TABLE_MANUFACTURERS_META . " WHERE manufacturers_id = " . (int)$manufacturer_id . " AND language_id = " . (int)$language_id . " LIMIT 1");
 
-  function zen_get_manufacturer_metatags_manu_description($manufacturer_id, $language_id) {
-    global $db;
-    $category = $db->Execute("select metatags_description
-                              from ".TABLE_MANUFACTURERS_META.
-                              " where manufacturers_id = '" . (int)$manufacturer_id . "'
-                              and language_id = '" . (int)$language_id . "'");
+  return ($category->EOF) ? '' : $category->fields['metatags_description'];
+}
 
-    return $category->fields['metatags_description'];
-  }
+function zen_get_manufacturer_metatags_keywords ($manufacturer_id, $language_id) {
+  global $db;
+  $category = $db->Execute ("SELECT * FROM " . TABLE_MANUFACTURERS_META . " WHERE manufacturers_id = " . (int)$manufacturer_id . " AND language_id = " . (int)$language_id . " LIMIT 1");
 
-  function zen_get_manufacturer_metatags_manu_keywords($manufacturer_id, $language_id) {
-    global $db;
-    $category = $db->Execute("select metatags_keywords
-                              from ".TABLE_MANUFACTURERS_META.
-                              " where manufacturers_id = '" . (int)$manufacturer_id . "'
-                              and language_id = '" . (int)$language_id . "'");
-
-    return $category->fields['metatags_keywords'];
-  }
-
-/**
- *
- */
- 
-?>
+  return ($category->EOF) ? '' : $category->fields['metatags_keywords'];
+}
