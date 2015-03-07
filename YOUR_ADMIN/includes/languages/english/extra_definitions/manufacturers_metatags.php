fe31a082be5a1097e0de6142a0e1e59417a629c4
lat9 <lat9@vinosdefrutastropicales.com>
2015-03-07 11:13:12 -0500

    #2: Correct admin-level constant
    Correct the misspelling in the admin-level extra_definitions file.

--- YOUR_ADMIN/includes/languages/english/extra_definitions/manufacturers_metatags.php
+++ YOUR_ADMIN/includes/languages/english/extra_definitions/manufacturers_metatags.php
@@ -2,7 +2,7 @@
 // -----
 // Part of the Manufacturers Meta Tags plugin for Zen Cart.
 //
-define('TEXT_INFO_HEADING_EDIT_CATEGORY_META_TAGS', 'Manufacturers Meta Tags Definitions');
+define('TEXT_INFO_HEADING_EDIT_MANUFACTURER_META_TAGS', 'Manufacturers Meta Tags Definitions');
 define('TEXT_EDIT_MANUFACTURER_META_TAGS_INTRO', 'Define Custom Meta Tags');
 define('TEXT_EDIT_MANUFACTURER_META_TAGS_TITLE', 'Title:');
 define('TEXT_EDIT_MANUFACTURER_META_TAGS_KEYWORDS', 'Keywords:');
