5b085a2f74525e00c0f631e0f68faac44b8b14df
lat9 <cindy@vinosdefrutastropicales.com>
2025-10-29 12:23:05 -0400

    Convert to encapsulated
    Ref: #5

--- YOUR_ADMIN/includes/languages/spanish/extra_definitions/manufacturers_metatags.php
+++
@@ -1,9 +0,0 @@
-<?php
-// -----
-// Part of the Manufacturers Meta Tags plugin for Zen Cart.
-//
-define('TEXT_INFO_HEADING_EDIT_MANUFACTURER_META_TAGS', 'Meta-tags de las Marcas');
-define('TEXT_EDIT_MANUFACTURER_META_TAGS_INTRO', 'Meta-tags por defecto');
-define('TEXT_EDIT_MANUFACTURER_META_TAGS_TITLE', 'T&iacute;tulo:');
-define('TEXT_EDIT_MANUFACTURER_META_TAGS_KEYWORDS', 'Keywords:');
-define('TEXT_EDIT_MANUFACTURERS_META_TAGS_DESCRIPTION', 'Description:');
---
+++ zc_plugins/ManufacturersMetatags/v3.0.0/manifest.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'pluginVersion' => 'v3.0.0',
+    'pluginName' => 'Manufacturers Metatags',
+    'pluginDescription' => 'This plugin enables your store to assign manufacturer-specific meta-tag title, keyword and/or description text, in a manner similar to the product-specific meta-tag processing that is built into Zen Cart.',
+    'pluginAuthor' => 'lat9',
+    'pluginId' => 932, // ID from Zen Cart forum
+    'zcVersions' => [],
+    'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
+    'github_repo' => '', // url
+    'pluginGroups' => [],
+];
